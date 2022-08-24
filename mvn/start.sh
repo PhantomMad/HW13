09ebcab1d57642aebc3e0f1941e67d463d7912c6
AndrewM <enigmatick@rambler.ru>
2022-08-24 20:43:45 +0300

    Prepare mysql

--- mvn/start.sh
+++ mvn/start.sh
@@ -3,11 +3,11 @@ db_conn="prj/WebContent/Config.properties"
 git clone ${GIT_URL} prj
 #
 if [ -e ${db_conn} ]; then
- cat <<<'app42.paas.db.username = "tomcat"
-app42.paas.db.port = "3306"
-app42.paas.db.password = "tomcat"
-app42.paas.db.ip = "dbsrv.loc"
-app42.paas.db.name = "dbtom"' > ${db_conn}
+ cat <<<'app42.paas.db.username = tomcat
+app42.paas.db.port = 3306
+app42.paas.db.password = tomcat
+app42.paas.db.ip = dbsrv.loc
+app42.paas.db.name = dbtom' > ${db_conn}
 fi
 #
 mvn package -f prj/pom.xml
