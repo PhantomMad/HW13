47c5eb139bcd8a520b8db7961b9ca6d8d2a27a9d
AndrewM <enigmatick@rambler.ru>
2022-08-24 21:15:30 +0300

    Prepare mysql

--- mvn/start.sh
+++ mvn/start.sh
@@ -6,7 +6,7 @@ if [ -e ${db_conn} ]; then
  cat <<<'app42.paas.db.username = tomcat
 app42.paas.db.port = 3306
 app42.paas.db.password = tomcat
-app42.paas.db.ip = dbsrv.loc
+app42.paas.db.ip = 10.115.10.120
 app42.paas.db.name = dbtom' > ${db_conn}
 fi
 #
