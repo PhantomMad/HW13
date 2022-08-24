f0e26371e6e4653888cba9f74b6dfe9d7da86b50
AndrewM <enigmatic@rambler.ru>
2022-08-24 10:31:40 +0300

    Prepare mysql

--- db/pginit/init-user-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
-	CREATE USER ${TOMCAT_USR} WITH PASSWORD ${TOMCAT_PW};
-	CREATE DATABASE ${TOMCAT_DB};
-	GRANT ALL PRIVILEGES ON DATABASE ${TOMCAT_DB} TO ${TOMCAT_USR};
-EOSQL
